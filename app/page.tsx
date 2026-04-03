62330c65c45ba9dbaf6cc8ab11288bf4f6456180
Dawood90999 <dawood90999@gmail.com>
2026-04-03 22:32:22 +0500

    feat: implement landing page flow with separate upload route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import ScanoLayout from "./components/ScanoLayout";
-import PdfUpload from "./components/PdfUpload";
+import LandingHero from "./components/LandingHero";
+import FeaturesSection from "./components/FeaturesSection";
 
 export default function Home() {
   return (
-    <ScanoLayout>
-      <PdfUpload />
-    </ScanoLayout>
+    <main className="min-h-screen">
+      <LandingHero />
+      <FeaturesSection />
+    </main>
   );
 }
---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import ScanoLayout from "../components/ScanoLayout";
+import PdfUpload from "../components/PdfUpload";
+
+export default function UploadPage() {
+  return (
+    <ScanoLayout>
+      <PdfUpload />
+    </ScanoLayout>
+  );
+}
